e843384d67be9bc10468dcbdefe49dfea45587d7
Komal Kumari <78696212+komalsah003@users.noreply.github.com>
2023-05-20 16:51:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ connectDB();
 
 //rest object
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = fileURLToPath(__filename);
+const __dirname = path.dirname(__filename);
 const app = express();
 
 //middlewares
